051c841c6b4eeca4abb1cf6eaea9f7fa6841a452
minhajspace <minhaj.space@gmail.com>
2019-11-14 13:37:16 +0530

    first commit

---
+++ src/Search.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div className="ui segment">
+        <from className=" ui form">
+          <div className="field">
+            <lable>Image Search</lable>
+            <input />
+          </div>
+        </from>
+      </div>
+    );
+  }
+}
+export default Search;
