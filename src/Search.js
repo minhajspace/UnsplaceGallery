9691cac32f8825684a2fc24bba2a2c70313cf1d6
minhajspace <minhaj.space@gmail.com>
2019-11-20 18:38:12 +0530

    second commit

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 class Search extends React.Component {
+  state = { term: "" };
+
   render() {
     return (
       <div className="ui segment">
         <from className=" ui form">
           <div className="field">
-            <lable>Image Search</lable>
-            <input />
+            <lable> Image Search </lable>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </from>
       </div>
