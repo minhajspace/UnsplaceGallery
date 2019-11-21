1f0f3e6fa55266104df366c49c641cc4b941f2ca
minhajspace <minhaj.space@gmail.com>
2019-11-21 22:27:30 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Search from "./Search";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <Search />
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <Search />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,28 @@
 import React from "react";
 
 class Search extends React.Component {
-  state = { term: "" };
+  state = {
+    inputValue: " "
+  };
+
+  onChange = event => {
+    this.setState({ inputValue: event.target.value });
+  };
+  onFormSubmit(event) {
+    event.prevent.defaut();
+  }
 
   render() {
     return (
-      <div className="ui segment">
-        <from className=" ui form">
+      <div className=" ui segment">
+        <from className="ui form " onSubmit={this.onFormSubmit}>
           <div className="field">
-            <lable> Image Search </lable>
+            <lable className="">Image Search</lable>
             <input
+              className=""
               type="text"
-              value={this.state.term}
-              onChange={e => this.setState({ term: e.target.value })}
+              // value={this.state.inputValue}
+              onChange={this.onChange}
             />
           </div>
         </from>
@@ -20,4 +30,30 @@ class Search extends React.Component {
     );
   }
 }
+
+//{
+
+//   state = { term: " " };
+
+//   onFromSubmit = event => {
+//     event.preventDefault();
+//   };
+
+//   render() {
+//     return (
+//       <div className="ui segment">
+//         <from className=" ui form" onSubmit={this.onFromSubmit}>
+//           <div className="field">
+//             <lable> Image Search </lable>
+//             <input
+//               type="text"
+//               value={this.state.term}
+//               onChange={event => this.setState({ term: event.target.value })}
+//             />
+//           </div>
+//         </from>
+//       </div>
+//     );
+//   }
+// }
 export default Search;
